fd28ef8ca756deea326d83a18d3ce7ca3e8eab55
mattdood <mawimberly@csumb.edu>
2020-05-10 14:54:00 -0700

    first commit

---
+++ cookiecutter_example/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from cookiecutter_example.users.models import User
+from cookiecutter_example.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
